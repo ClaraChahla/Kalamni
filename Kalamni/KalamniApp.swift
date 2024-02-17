6e9b399c2fecfb8f5cf3bfaad9d975d307d0082a
Daniel Camacho <danccamacho@gmail.com>
2024-02-17 14:15:06 -0800

    Initial Commit

---
+++ Kalamni/KalamniApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalamniApp.swift
+//  Kalamni
+//
+//  Created by csuftitan on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct KalamniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
